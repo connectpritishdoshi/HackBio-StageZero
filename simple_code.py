08c9ac6bfd47d809f299560101d58b6fb1c78b93
connectpritishdoshi <connectpritish@gmail.com>
2026-04-23 17:16:50 +0530

    last commit

--- simple_code.py
+++ simple_code.py
@@ -1,7 +1,7 @@
 # HackBio Stage Zero: Python Script
 # Requirement: Write a simple Python script for printing the names, affiliation, and the name of the genes you love most, and the name of the organism bearing the gene. The final output should be something like, “Hi, my name is Adewale Ogunleye, a researcher at the University of Tübingen. My favorite gene is lexA in Escherichia coli.”
 
-name = "Pritish Doshi"
+name = "Pritish Doshi" #Gauri Jagtap
 affiliation = "University of Mumbai"
 favorite_gene = "BRCA2" #Repairs damaged DNA
 organism = "Homo sapiens"
